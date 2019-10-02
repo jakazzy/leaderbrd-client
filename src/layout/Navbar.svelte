42c615c806af7c4edc28105ce29ef02ccbd3aed7
jakazzy <jakazzy@gmail.com>
2019-10-02 08:07:53 +0000

    create navbar

---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<nav>
+  <div class="nav-wrapper">
+    <Link to="/" class="brand-logo">Notes App</Link>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+
+    </ul>
+  </div>
+</nav>
