69c14843375829b1c5ac8e0f8196a7a9093da657
jakazzy <jakazzy@gmail.com>
2019-10-02 08:38:10 +0000

    posts

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,7 +1,29 @@
 <script>
   import { onMount } from "svelte";
   const apiBaseUrl =
-    "https://https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev";
+    "https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev";
+
+  let posts = [];
+  onMount(async () => {
+    const res = await fetch(apiBaseUrl + "/posts");
+    posts = await res.json();
+  });
 </script>
 
-<!-- <h1>Home Page</h1> -->
+<div class="row">
+  {#if posts.length === 0}
+    <h2>Loading posts...</h2>
+  {:else}
+    {#each posts as post}
+      <div class="col s6">
+        <div class="card">
+          <div class="card-content">
+            <p class="card-title">{post.title}</p>
+            <p>{post.createdAt}</p>
+            <p>{post.body}</p>
+          </div>
+        </div>
+      </div>
+    {/each}
+  {/if}
+</div>
