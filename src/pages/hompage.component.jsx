97122f6718fba8d09bc6aa60686ea9b52a976c1d
jakazzy <jakazzy@gmail.com>
2020-06-24 16:50:43 +0000

    add header

---
+++ src/pages/hompage.component.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import HeaderComponent from '../components/header.component';
+
+// function HomePage() {
+//   return (
+//     <div>
+//       <HeaderComponent />
+//     </div>
+//   );
+// }
+
+// export default HomePage;
