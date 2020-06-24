ca62eadcdfa2fda514ebbb814c0dd19125c4c16b
jakazzy <jakazzy@gmail.com>
2020-06-24 16:56:12 +0000

    change navbar

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <nav className="flex items-center justify-between flex-wrap bg-red-900 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <span className="font-semibold text-xl tracking-tight">
             Leader Board
