ef432336a373c59a3029bdc75e29fea4cb779a43
jakazzy <jakazzy@gmail.com>
2020-06-24 11:12:48 +0000

    add random card

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <img src="https://source.unsplash.com/random" 
+    alt="alternative images"
+    className="w-full"/>
+  </div>;
+}
+
+export default App;
