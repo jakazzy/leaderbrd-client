ee5bb032feebcd4a9827d6ffb282b971a6b85357
jakazzy <jakazzy@gmail.com>
2020-06-24 11:57:54 +0000

    add linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img src="https://source.unsplash.com/random" 
-    alt="alternative images"
-    className="w-full"/>
-  </div>;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img
+        src="https://source.unsplash.com/random"
+        alt="alternative images"
+        className="w-full"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+            300
+          </li>
+          <li>
+            <strong>Likes:</strong>
+            400
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
