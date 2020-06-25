d5a7e72c13bb4fee3ec29130e15f455d3d613f1c
jakazzy <jakazzy@gmail.com>
2020-06-25 10:36:47 +0000

    make changes to the UI

--- src/components/table.component.jsx
+++ src/components/table.component.jsx
@@ -68,13 +68,13 @@ const Table = () => {
                       User
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      Rol
+                      @slack
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      Created at
+                      Track
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      Status
+                      Score
                     </th>
                   </tr>
                 </thead>
