408077c524931505709a9ae72cd02ade0edd173c
jakazzy <jakazzy@gmail.com>
2020-06-25 16:27:59 +0000

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Header from './components/header.component';
 import HomePage from './pages/home-page.component';
+import LoginPage from './pages/login-page.component';
+import UploadPage from './pages/upload-page.component';
 
 function App() {
   return (
     <div className="antialiased" bg-gray-200 text-gray-900>
       <Header />
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/upload" component={UploadPage} />
+      </Switch>
     </div>
   );
 }
