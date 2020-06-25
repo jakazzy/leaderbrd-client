478ce759ef4a452c3824ef6276f08a325455b7ed
jakazzy <jakazzy@gmail.com>
2020-06-25 06:05:56 +0000

    add search input field and button

--- src/components/searchbar.component.jsx
+++ src/components/searchbar.component.jsx
@@ -3,13 +3,21 @@ import PropTypes from 'prop-types';
 
 const SearchBar = ({ search, handleChange, handleSearch }) => {
   return (
-    <form onSubmit={handleSearch}>
-      <input
-        className=""
-        value={search}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-      <button type="submit">submit</button>
+    <form onSubmit={handleSearch} className="w-full mb-8">
+      <div className="flex items-center  border-b-2 border-red-700 py-2">
+        <input
+          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          value={search}
+          onChange={(e) => handleChange(e.target.value)}
+          placeholder="Enter a search query..."
+        />
+        <button
+          type="submit"
+          className="flex-shrink-0 bg-red-900 hover:bg-red-700 border-red-900 hover:border-red-700 text-sm border-4 text-white py-2 px-8 rounded"
+        >
+          Search
+        </button>
+      </div>
     </form>
   );
 };
