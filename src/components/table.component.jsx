9142c0d63da2473cfd1787e5678f3965a95f2bc4
jakazzy <jakazzy@gmail.com>
2020-06-25 09:20:11 +0000

    edit the options

--- src/components/table.component.jsx
+++ src/components/table.component.jsx
@@ -29,8 +29,9 @@ const Table = () => {
               <div className="relative">
                 <select className="h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
                   <option>All</option>
-                  <option>Active</option>
-                  <option>Inactive</option>
+                  <option>Design</option>
+                  <option>Backend</option>
+                  <option>Frontend</option>
                 </select>
                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                   <svg
