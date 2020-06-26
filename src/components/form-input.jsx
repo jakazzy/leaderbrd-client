7995fc27f04d95185f688411dba671ec3f155dd7
jakazzy <jakazzy@gmail.com>
2020-06-26 06:03:47 +0000

    add link to form

--- src/components/form-input.jsx
+++ src/components/form-input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FormInput = () => {
   return (
-    <div className="">
+    <div className="mb-4 mt-6">
       <label
         htmlFor="email"
         className="block text-gray-700 text-sm font-semibold mb-2"
@@ -10,7 +10,7 @@ const FormInput = () => {
         {' '}
         Email
         <input
-          className="block w-11/12 h-10"
+          className="block w-full h-10 text-sm  rounded  mt-4 py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline"
           type="email"
           name="email"
           id="email"
--- src/pages/login-page.component.jsx
+++ src/pages/login-page.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FormInput from '../components/form-input';
 import CustomButton from '../components/custom-button.component';
 
 const LoginPage = () => {
   return (
     <div className="container mx-auto h-screen">
-      <form className="w-1/2 mx-auto rounded border border-gray-400 shadow-lg my-20">
+      <form className="w-1/2 mx-auto rounded border border-gray-400 shadow-lg my-20 px-8">
         <div>
           <h1 className=" text-2xl font-bold">Admin Sign in</h1>
           <div>
@@ -25,6 +26,12 @@ const LoginPage = () => {
           placeholder="............"
           label="password"
         />
+        <Link
+          className="inline-block align-baseline text-sm text-gray-600 hover:text-gray-800"
+          to="/forgot"
+        >
+          Forgot Password?
+        </Link>
         <CustomButton />
       </form>
     </div>
